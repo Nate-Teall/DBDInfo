0e7d4dc9d72cfb14e306bcdadf6274dc959208dc
Nate-Teall <nnt2913@rit.edu>
2024-06-22 17:18:52 -0400

    Added scream counter command

--- dbd.py
+++ dbd.py
@@ -1,24 +1,22 @@
 import requests
 import json
 
-DBD_URL = "https://dbd.tricky.lol/api/"
-
 # Handles requests to the dbd.tricky.lol API
 class DbdApi:
 
-    __slots__ = ["player_data", "player_name"]
+    __slots__ = ["DBD_URL"]
 
     def __init__(self):
-        self.player_data = None
-        self.player_name = None
+        self.DBD_URL = "https://dbd.tricky.lol/api/"
 
     # makes the request for player data, given the user's SteamID64 as a string
     # should be by player's steam name (soon)
-    def get_player_stats(self, player_id):
-        data = requests.get(DBD_URL + "playerstats?steamid=" + player_id)
+    def get_player_screams(self, player_id):
+        response = requests.get(self.DBD_URL + "playerstats?steamid=" + player_id)
 
-        if data.status_code == 404:
+        if response.status_code == 404:
             raise ValueError("Error: User", player_id, "not found.") 
 
-        self.player_data = json.loads(data.text)
-        self.player_name = player_id
+        data = json.loads(response.text)
+        
+        return data["screams"]
--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import discord
 import os
 from dbd import DbdApi
 
-DBD = None
+DBD = DbdApi()
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}!') 
-        DBD = DbdApi()
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        if message.content.startswith("-hello"):
-            await message.channel.send("sup")
+
+        if message.content.startswith("-"):
+            params = message.content.split()
+
+            match params[0]:
+                case "-screams": 
+                    try:
+                        scream_count = DBD.get_player_screams(params[0])
+                        await message.channel.send("Yikes! You've screamed", scream_count, "times!")
+
+                    except ValueError:
+                        await message.channel.send("Player", params[0], "not found!")
+                    
+                case _: 
+                    return
 
 intents = discord.Intents.default()
 intents.message_content = True
