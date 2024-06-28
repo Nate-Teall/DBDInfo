b74cedbc5efcfd30e5a3c440fd66df702ce1125a
Nate-Teall <nnt2913@rit.edu>
2024-06-27 23:03:46 -0400

    Stopped commiting RunContainer.sh

--- RunContainer.sh
+++
@@ -1,2 +0,0 @@
-docker run -it -d -e DISCORD_TOKEN=$DISCORD_TOKEN -e STEAM_API_KEY=$STEAM_API_KEY --mount type=bind,source="$(pwd)"/,target=/usr/local/DBDInfo --name discord-bot-container nateteall/dbd-info
-docker exec -it discord-bot-container sh
