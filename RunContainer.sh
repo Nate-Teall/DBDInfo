5606cc7f5ea207096e07d2c99195fde088405f23
Nate-Teall <nnt2913@rit.edu>
2024-06-27 23:01:33 -0400

    More gitignore updates

--- RunContainer.sh
+++ RunContainer.sh
@@ -1,5 +1,2 @@
-docker rmi nateteall/dbd-info
-docker build -t nateteall/dbd-info .
-
-docker run -it -d -e DISCORD_TOKEN=$DISCORD_TOKEN -e STEAM_API_KEY=$STEAM_API_KEY --name discord-bot-container nateteall/dbd-info
+docker run -it -d -e DISCORD_TOKEN=$DISCORD_TOKEN -e STEAM_API_KEY=$STEAM_API_KEY --mount type=bind,source="$(pwd)"/,target=/usr/local/DBDInfo --name discord-bot-container nateteall/dbd-info
 docker exec -it discord-bot-container sh
