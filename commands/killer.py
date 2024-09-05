d22b44665df60df42981a68cb23fdac9f138d4b3
Nate-Teall <nnt2913@rit.edu>
2024-09-05 10:23:27 -0400

    Finished killer stats command

--- commands/killer.py
+++ commands/killer.py
@@ -30,12 +30,34 @@ class Killer:
         kills_on_hook = player_stats["DBD_SacrificedCampers"] if "DBD_SacrificedCampers" in player_stats else 0
         kills_by_mori = player_stats["DBD_KilledCampers"] if "DBD_KilledCampers" in player_stats else 0
         games_won_quick = player_stats["DBD_Chapter11_Slasher_Stat1"] if "DBD_Chapter11_Slasher_Stat1" in player_stats else 0
-        basement_hooks = player_stats["DBD_DLC6_Slasher_Stat2"] if "DBD_DLC6_Slasher_Stat1" in player_stats else 0
+        basement_hooks = player_stats["DBD_DLC6_Slasher_Stat2"] if "DBD_DLC6_Slasher_Stat2" in player_stats else 0
         hatches_closed = player_stats["DBD_Chapter13_Slasher_Stat1"] if "DBD_Chapter13_Slasher_Stat1" in player_stats else 0
 
         embed = self.UTILS.make_embed(self.UTILS.Color.KILLER)
         embed.title = "Killer stats for: " + vanity_url
         embed.set_thumbnail(url=player_pfp_url)
+
+        embed.add_field(
+            name="Kills on Hook:",
+            value=kills_on_hook)
+        
+        embed.add_field(
+            name="Kills not on Hook:",
+            value=kills_by_mori)
+        
+        embed.add_field(
+            name="Games Won Before Last Gen Completed:",
+            value=games_won_quick)
+        
+        embed.add_field(
+            name="Hatches Closed:",
+            value=hatches_closed,
+            inline=True)
+        
+        embed.add_field(
+            name="Survivors Hooked in the Basement:",
+            value=basement_hooks,
+            inline=True)
 
         response[1] = embed
         return response
